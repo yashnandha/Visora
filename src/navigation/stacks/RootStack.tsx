c761a21310f0b53b427832b2af0e61ba85bbace1
yashnandha <teamneheart@gmail.com>
2026-06-21 14:47:34 +0530

    Add Camera Page

--- src/navigation/stacks/RootStack.tsx
+++ src/navigation/stacks/RootStack.tsx
@@ -1,3 +1,4 @@
+import CameraScreen from "@screens/CameraScreen/CameraScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RootStackParams } from "../rootStackParams";
 
@@ -5,12 +6,13 @@ const Stack = createNativeStackNavigator<RootStackParams>();
 const RootStack = () => {
   return (
     <Stack.Navigator
+      initialRouteName="camera"
       screenOptions={{
         headerShown: false,
         gestureEnabled: true,
         animation: "none",
       }}>
-
+      <Stack.Screen name="camera" component={CameraScreen} />
     </Stack.Navigator>
   );
 };
